22187832e24c5d16d6e0a088c9c9d571b7d69053
Matija Lukic <matija.lukic003@gmail.com>
2026-07-02 15:49:30 +0200

    Added enums

---
+++ ParkingTicektsManagment.Domain/Domains/Enums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingTicektsManagment.Domain.Domains
+{
+    public enum Role
+    {
+        Admin = 1,
+        Worker = 2,
+        User = 3
+    }
+
+    public enum TicketStatus
+    {
+        Unpaid = 1,
+        Paid = 2
+    }
+}
