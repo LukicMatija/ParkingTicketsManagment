23e89e86e612dfaaf824cfcb025b93a311d96104
Matija Lukic <matija.lukic003@gmail.com>
2026-07-02 15:51:00 +0200

    Added User class

---
+++ ParkingTicektsManagment.Domain/Domains/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ParkingTicektsManagment.Domain.Domains
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [Required]
+        public Role Role { get; set; }
+
+        public ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public ICollection<ParkingTicket> IssuedTickets { get; set; } = new List<ParkingTicket>();
+    }
+}
