f0fe17589108bec42a11be34ee04f88a3f79a673
Matija Lukic <matija.lukic003@gmail.com>
2026-07-02 16:00:37 +0200

    Added ParkingTicket class

---
+++ ParkingTicektsManagment.Domain/Domains/ParkingTicket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ParkingTicektsManagment.Domain.Domains
+{
+    public class ParkingTicket
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string TicketNumber { get; set; } = string.Empty;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0.00, 50000.00)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime IssuedAt { get; set; }
+
+        [Required]
+        public TicketStatus Status { get; set; }
+
+        [Required]
+        public Guid VehicleId { get; set; }
+
+        [Required]
+        public Guid LocationId { get; set; }
+
+        [Required]
+        public Guid WorkerId { get; set; }
+
+        public Vehicle Vehicle { get; set; } = null!;
+        public Location Location { get; set; } = null!;
+
+        [ForeignKey(nameof(WorkerId))]
+        public User Worker { get; set; } = null!;
+
+        public ICollection<Payment> Payments { get; set; } = new List<Payment>();
+    }
+}
