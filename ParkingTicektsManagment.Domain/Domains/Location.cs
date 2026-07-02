1c8d3eaae106fdb49a6e06fea1a2074cd0cf88c6
Matija Lukic <matija.lukic003@gmail.com>
2026-07-02 15:58:09 +0200

    Added Location class

---
+++ ParkingTicektsManagment.Domain/Domains/Location.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using NetTopologySuite.Geometries;
+
+
+namespace ParkingTicektsManagment.Domain.Domains
+{
+    public class Location
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string StreetName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(50)]
+        public string City { get; set; } = string.Empty;
+
+        [Required]
+        public Guid ZoneId { get; set; }
+
+        [Required]
+        public Point Coordinates { get; set; } = null!;
+
+        public Zone Zone { get; set; } = null!;
+        public ICollection<ParkingTicket> ParkingTickets { get; set; } = new List<ParkingTicket>();
+    }
+}
+}
