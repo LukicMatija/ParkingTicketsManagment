f1f7481919bb04033100214201516396bce7d90e
Matija Lukic <matija.lukic003@gmail.com>
2026-07-03 14:50:02 +0200

    Added UnitOfWork

---
+++ ParkingTicektsManagment.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingTicektsManagment.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+
+        int SaveChanges();
+    }
+}
---
+++ ParkingTicketsManagement.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using ParkingTicektsManagment.Domain.Repositories;
+using ParkingTicketsManagement.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingTicketsManagement.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+        public int SaveChanges() => _context.SaveChanges();
+
+        public void Dispose() => _context.Dispose();
+
+    }
+}
